1aae3d43f1ff0949a659b3b0b769cae20a92c87c
Alish Palasara <palasaraalis@gmail.com>
2024-12-01 15:53:01 +0530

    remove map library

--- src/sections/contact/view/contact-view.jsx
+++ src/sections/contact/view/contact-view.jsx
@@ -1,9 +1,6 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
-import { _mapContact } from 'src/_mock';
-
-import { ContactMap } from '../contact-map';
 import { ContactHero } from '../contact-hero';
 import { ContactForm } from '../contact-form';
 
@@ -21,8 +18,6 @@ export function ContactView() {
           gridTemplateColumns={{ xs: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
         >
           <ContactForm />
-
-          <ContactMap contacts={_mapContact} />
         </Box>
       </Container>
     </>
