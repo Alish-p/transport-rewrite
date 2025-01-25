5334fb524ce830010b1e211f53ee5c5e63cc80ea
Alish Palasara <palasaraalis@gmail.com>
2025-01-25 10:18:39 +0530

    fix: double logo apearing bug fix

--- src/sections/subtrip/pdfs/entry-pass-pdf.jsx
+++ src/sections/subtrip/pdfs/entry-pass-pdf.jsx
@@ -44,7 +44,7 @@ export default function EntryPassPdf({ subtrip }) {
     <View style={[styles.gridContainer, styles.border]}>
       <View style={[styles.gridContainer, styles.col8, styles.p8, styles.borderRight]}>
         <View style={[styles.col4]}>
-          <Image source="/logo/company-logo.png" style={{ width: 48, height: 48 }} />
+          <Image source="/logo/company-logo-green.png" style={{ width: 48, height: 48 }} />
         </View>
 
         <View style={[styles.col8, { display: 'flex', alignItems: 'center' }]}>
