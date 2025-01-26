a1eb11b19aab0abaa752513f5092b7fbd02b4cf6
Alish Palasara <palasaraalis@gmail.com>
2025-01-26 14:33:26 +0530

    fix active link, driver-salary was also matching with driver section so

--- src/routes/hooks/use-active-link.js
+++ src/routes/hooks/use-active-link.js
@@ -2,7 +2,6 @@ import { usePathname } from './use-pathname';
 import { hasParams, removeParams, isExternalLink, removeLastSlash } from '../utils';
 
 // ----------------------------------------------------------------------
-
 export function useActiveLink(itemPath, deep = true) {
   const pathname = removeLastSlash(usePathname());
 
@@ -16,40 +15,22 @@ export function useActiveLink(itemPath, deep = true) {
   }
   /* End check */
 
-  /**
-   * [1] Apply for Item has children or has params.
-   */
   const isDeep = deep || pathHasParams;
 
-  // console.info(isDeep ? '[deep]   :' : '[normal] :', itemPath, '-?-', pathname);
-
   if (isDeep) {
-    /**
-     * [1] Deep: default
-     * @itemPath 			 = '/dashboard/user'
-     * @match pathname = '/dashboard/user'
-     * @match pathname = '/dashboard/user/list'
-     * @match pathname = '/dashboard/user/e99f09a7-dd88-49d5-b1c8-1daf80c2d7b15/edit'
-     */
-    const defaultActive = pathname.includes(itemPath);
+    // Exact match for the path
+    const exactMatch = pathname === itemPath;
 
-    /**
-     * [1] Deep: has params
-     * @itemPath 			 = '/dashboard/test?id=e99f09a7-dd88-49d5-b1c8-1daf80c2d7b1'
-     * @match pathname = '/dashboard/test'
-     */
+    // Ensure the itemPath is a parent path with a valid `/` separator
+    const parentMatch = pathname.startsWith(`${itemPath}/`) || pathname === itemPath;
 
+    // Deep match for params
     const originItemPath = removeParams(itemPath);
-
     const hasParamsActive = pathHasParams && originItemPath === pathname;
 
-    return defaultActive || hasParamsActive;
+    return exactMatch || parentMatch || hasParamsActive;
   }
 
-  /**
-   * [1] Normal: active
-   * @itemPath 			 = '/dashboard/calendar'
-   * @match pathname = '/dashboard/calendar'
-   */
+  // For shallow match, ensure exact match
   return pathname === itemPath;
 }
