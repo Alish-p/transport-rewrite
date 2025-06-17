2a155ab2df3cbb7650f226a241c721d897a78d6c
Alish Palasara <palasaraalis@gmail.com>
2025-06-17 16:08:19 +0530

    driver kanaban fix

--- src/sections/kanban/components/kanban-driver-dialog.jsx
+++ src/sections/kanban/components/kanban-driver-dialog.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable consistent-return */
 import { useState, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
