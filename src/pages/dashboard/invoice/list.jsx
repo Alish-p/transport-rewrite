c30294388d1d2cb8de60cf9c8ea78eae3a2d0b5a
Alish Palasara <palasaraalis@gmail.com>
2025-06-29 01:00:51 +0530

    invoice pagination

--- src/pages/dashboard/invoice/list.jsx
+++ src/pages/dashboard/invoice/list.jsx
@@ -1,10 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 
 import { CONFIG } from 'src/config-global';
-import { useInvoices } from 'src/query/use-invoice';
-
-import { EmptyContent } from 'src/components/empty-content';
-import { LoadingScreen } from 'src/components/loading-screen';
 
 import { InvoiceListView } from 'src/sections/invoice/views';
 
@@ -13,23 +9,13 @@ import { InvoiceListView } from 'src/sections/invoice/views';
 const metadata = { title: `Invoice list | Dashboard - ${CONFIG.site.name}` };
 
 export default function Page() {
-  const { data: invoices, isLoading, isError } = useInvoices();
-
-  if (isLoading) {
-    return <LoadingScreen />;
-  }
-
-  if (isError) {
-    return <EmptyContent />;
-  }
-
   return (
     <>
       <Helmet>
         <title> {metadata.title}</title>
       </Helmet>
 
-      <InvoiceListView invoices={invoices} />
+      <InvoiceListView />
     </>
   );
 }
