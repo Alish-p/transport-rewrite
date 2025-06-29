43238cd0ad42c515d1d8c8dc52281d40bfcaa55c
Alish Palasara <palasaraalis@gmail.com>
2025-06-29 16:20:48 +0530

    added edit and delete in table row

--- src/sections/subtrip/active-list/subtrip-table-row.jsx
+++ src/sections/subtrip/active-list/subtrip-table-row.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 
 // @mui
 import { MenuList } from '@mui/material';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
 import MenuItem from '@mui/material/MenuItem';
 import TableRow from '@mui/material/TableRow';
 import Checkbox from '@mui/material/Checkbox';
@@ -10,14 +12,26 @@ import TableCell from '@mui/material/TableCell';
 import IconButton from '@mui/material/IconButton';
 import ListItemText from '@mui/material/ListItemText';
 
+import { useBoolean } from 'src/hooks/use-boolean';
+
 import { Iconify } from 'src/components/iconify';
+import { ConfirmDialog } from 'src/components/custom-dialog';
 import { usePopover, CustomPopover } from 'src/components/custom-popover';
 
 import { TABLE_COLUMNS } from '../config/table-columns';
 
 // ----------------------------------------------------------------------
 
-export default function SubtripTableRow({ row, selected, onSelectRow, onViewRow, visibleColumns }) {
+export default function SubtripTableRow({
+  row,
+  selected,
+  onSelectRow,
+  onViewRow,
+  onEditRow,
+  onDeleteRow,
+  visibleColumns,
+}) {
+  const confirm = useBoolean();
   const popover = usePopover();
 
   const renderCellContent = (columnConfig, rowData) => {
@@ -78,8 +92,43 @@ export default function SubtripTableRow({ row, selected, onSelectRow, onViewRow,
             <Iconify icon="solar:eye-bold" />
             View
           </MenuItem>
+
+          <MenuItem
+            onClick={() => {
+              onEditRow(row._id);
+              popover.onClose();
+            }}
+          >
+            <Iconify icon="solar:pen-bold" />
+            Edit
+          </MenuItem>
+
+          <Divider sx={{ borderStyle: 'dashed' }} />
+
+          <MenuItem
+            onClick={() => {
+              confirm.onTrue();
+              popover.onClose();
+            }}
+            sx={{ color: 'error.main' }}
+          >
+            <Iconify icon="solar:trash-bin-trash-bold" />
+            Delete
+          </MenuItem>
         </MenuList>
       </CustomPopover>
+
+      <ConfirmDialog
+        open={confirm.value}
+        onClose={confirm.onFalse}
+        title="Delete"
+        content="Are you sure want to delete?"
+        action={
+          <Button variant="contained" color="error" onClick={() => onDeleteRow(row._id)}>
+            Delete
+          </Button>
+        }
+      />
     </>
   );
 }
