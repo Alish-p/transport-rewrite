6920d13619946a19275e9a3c4c455f8aafd8f703
Alish Palasara <palasaraalis@gmail.com>
2025-07-08 18:15:42 +0530

    minor fixes

--- src/sections/expense/hooks/use-visible-columns.js
+++
@@ -1,7 +0,0 @@
-import { useColumnVisibility } from 'src/hooks/use-column-visibility';
-
-import { TABLE_COLUMNS } from '../expense-table-config';
-
-export function useVisibleColumns() {
-  return useColumnVisibility(TABLE_COLUMNS);
-}
--- src/sections/transporter/hooks/use-visible-columns.js
+++
@@ -1,7 +0,0 @@
-import { useColumnVisibility } from 'src/hooks/use-column-visibility';
-
-import { TABLE_COLUMNS } from '../transporter-table-config';
-
-export function useVisibleColumns() {
-  return useColumnVisibility(TABLE_COLUMNS);
-}
