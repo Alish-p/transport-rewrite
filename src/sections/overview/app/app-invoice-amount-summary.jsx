38f8b47c04c08dd5ff7f4f8c5990802d205b45da
Alish Palasara <palasaraalis@gmail.com>
2025-07-13 17:51:56 +0530

    added scrollbar to summary

--- src/sections/overview/app/app-invoice-amount-summary.jsx
+++ src/sections/overview/app/app-invoice-amount-summary.jsx
@@ -5,6 +5,8 @@ import Divider from '@mui/material/Divider';
 import { useTheme } from '@mui/material/styles';
 import CardHeader from '@mui/material/CardHeader';
 
+import { Scrollbar } from 'src/components/scrollbar';
+
 import { fShortenNumber } from 'src/utils/format-number';
 
 import { Iconify } from 'src/components/iconify';
@@ -40,23 +42,25 @@ export function AppInvoiceAmountSummary({ summary, ...other }) {
   return (
     <Card {...other}>
       <CardHeader title="Invoice Summary" sx={{ mb: 2 }} />
-      <Stack
-        direction="row"
-        alignItems="center"
-        justifyContent="space-around"
-        divider={<Divider orientation="vertical" flexItem sx={{ borderStyle: 'dashed' }} />}
-        sx={{ px: 3, py: 2 }}
-      >
-        {ITEMS.map((item) => (
-          <Stack key={item.title} spacing={1} alignItems="center" sx={{ minWidth: 120 }}>
-            <Iconify icon={item.icon} width={24} sx={{ color: item.color }} />
-            <Box sx={{ typography: 'subtitle2', color: 'text.secondary', textAlign: 'center' }}>
-              {item.title}
-            </Box>
-            <Box sx={{ typography: 'h6' }}>₹ {fShortenNumber(item.amount)}</Box>
-          </Stack>
-        ))}
-      </Stack>
+      <Scrollbar>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-around"
+          divider={<Divider orientation="vertical" flexItem sx={{ borderStyle: 'dashed' }} />}
+          sx={{ px: 3, py: 2 }}
+        >
+          {ITEMS.map((item) => (
+            <Stack key={item.title} spacing={1} alignItems="center" sx={{ minWidth: 120 }}>
+              <Iconify icon={item.icon} width={24} sx={{ color: item.color }} />
+              <Box sx={{ typography: 'subtitle2', color: 'text.secondary', textAlign: 'center' }}>
+                {item.title}
+              </Box>
+              <Box sx={{ typography: 'h6' }}>₹ {fShortenNumber(item.amount)}</Box>
+            </Stack>
+          ))}
+        </Stack>
+      </Scrollbar>
     </Card>
   );
 }
