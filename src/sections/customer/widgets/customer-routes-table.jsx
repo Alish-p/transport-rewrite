f17c0d920e1e4fabcbf8a67fe8651040d3676fe8
Alish Palasara <palasaraalis@gmail.com>
2025-08-09 19:11:08 +0530

    added view less in table

--- src/sections/customer/widgets/customer-routes-table.jsx
+++ src/sections/customer/widgets/customer-routes-table.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
+
+import Box from '@mui/material/Box';
 import { Link } from '@mui/material';
 import Card from '@mui/material/Card';
 import Table from '@mui/material/Table';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
 import Tooltip from '@mui/material/Tooltip';
 import TableRow from '@mui/material/TableRow';
 import TableBody from '@mui/material/TableBody';
@@ -12,18 +17,21 @@ import { RouterLink } from 'src/routes/components';
 
 import { useCustomerRoutes } from 'src/query/use-customer';
 
+import { Iconify } from 'src/components/iconify';
 import { Scrollbar } from 'src/components/scrollbar';
 import { TableNoData, TableSkeleton, TableHeadCustom } from 'src/components/table';
 
 export function CustomerRoutesTable({ customer, title, subheader, ...other }) {
   const { _id: customerId } = customer || {};
   const { data: routes = [], isLoading } = useCustomerRoutes(customerId);
+  const [showAll, setShowAll] = useState(false);
+  const displayedRoutes = showAll ? routes : routes.slice(0, 6);
 
   return (
     <Card {...other}>
       <CardHeader title={title} subheader={subheader} sx={{ mb: 3 }} />
 
-      <Scrollbar sx={{ minHeight: 402, maxHeight: 402 }}>
+      <Scrollbar sx={{ minHeight: 402, ...(showAll && { maxHeight: 402 }) }}>
         <Table sx={{ minWidth: 680 }}>
           <TableHeadCustom
             headLabel={[
@@ -39,7 +47,7 @@ export function CustomerRoutesTable({ customer, title, subheader, ...other }) {
               <TableSkeleton />
             ) : routes.length ? (
               <>
-                {routes.map((row, idx) => (
+                {displayedRoutes.map((row, idx) => (
                   <TableRow key={row.routeId}>
                     <TableCell>{idx + 1}</TableCell>
                     <TableCell>
@@ -71,6 +79,29 @@ export function CustomerRoutesTable({ customer, title, subheader, ...other }) {
           </TableBody>
         </Table>
       </Scrollbar>
+
+      {routes.length > 6 && (
+        <>
+          <Divider sx={{ borderStyle: 'dashed' }} />
+
+          <Box sx={{ p: 2, textAlign: 'right' }}>
+            <Button
+              size="small"
+              color="inherit"
+              onClick={() => setShowAll((prev) => !prev)}
+              endIcon={
+                <Iconify
+                  icon={showAll ? 'eva:arrow-ios-upward-fill' : 'eva:arrow-ios-forward-fill'}
+                  width={18}
+                  sx={{ ml: -0.5 }}
+                />
+              }
+            >
+              {showAll ? 'View less' : 'View all'}
+            </Button>
+          </Box>
+        </>
+      )}
     </Card>
   );
 }
