3047c4c26ec4cbc713c691c299c0024d76bdcfdc
Alish Palasara <palasaraalis@gmail.com>
2025-08-10 02:51:54 +0530

    minor fix

--- src/sections/driver/widgets/index.js
+++ src/sections/driver/widgets/index.js
@@ -1,4 +1,4 @@
 export { InfoItem } from './info-item';
 export { DriverBasicWidget } from './driver-basic-widget';
-export { DriverAdditionalWidget } from './driver-additional-widget';
 export { DriverFinanceWidget } from './driver-finance-widget';
+export { DriverAdditionalWidget } from './driver-additional-widget';
