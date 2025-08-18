7d1ba0b5cc362039a4a511c40ccf8a5bb62c4725
Alish Palasara <palasaraalis@gmail.com>
2025-08-19 02:04:38 +0530

    added diesel price widget

--- src/sections/pump/widgets/index.js
+++ src/sections/pump/widgets/index.js
@@ -2,3 +2,4 @@ export { InfoItem } from './info-item';
 export { PumpBasicWidget } from './pump-basic-widget';
 export { PumpFinanceWidget } from './pump-finance-widget';
 export { PumpExpensesWidget } from './pump-expenses-widget';
+export { PumpDieselPriceWidget } from './pump-diesel-price-widget';
