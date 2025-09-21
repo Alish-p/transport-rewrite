a92d4c6e14b08e1cee92b4c62695ca8c62a247ad
Alish Palasara <palasaraalis@gmail.com>
2025-09-21 13:02:36 +0530

    maintanance mode fix

--- src/routes/sections/index.jsx
+++ src/routes/sections/index.jsx
@@ -18,48 +18,48 @@ const HomePage = lazy(() => import('src/pages/home'));
 const MaintenancePage = lazy(() => import('src/pages/maintenance'));
 
 export function Router() {
-  if (CONFIG.maintenanceMode) {
-    return useRoutes([
-      {
-        path: '*',
-        element: (
-          <Suspense fallback={<SplashScreen />}>
-            <SimpleLayout>
-              <MaintenancePage />
-            </SimpleLayout>
-          </Suspense>
-        ),
-      },
-    ]);
-  }
+  const routes = CONFIG.maintenanceMode
+    ? [
+        {
+          path: '*',
+          element: (
+            <Suspense fallback={<SplashScreen />}>
+              <SimpleLayout>
+                <MaintenancePage />
+              </SimpleLayout>
+            </Suspense>
+          ),
+        },
+      ]
+    : [
+        {
+          path: '/',
+          /**
+           * Skip home page
+           * element: <Navigate to={CONFIG.auth.redirectPath} replace />,
+           */
+          element: (
+            <Suspense fallback={<SplashScreen />}>
+              <MainLayout>
+                <HomePage />
+              </MainLayout>
+            </Suspense>
+          ),
+        },
 
-  return useRoutes([
-    {
-      path: '/',
-      /**
-       * Skip home page
-       * element: <Navigate to={CONFIG.auth.redirectPath} replace />,
-       */
-      element: (
-        <Suspense fallback={<SplashScreen />}>
-          <MainLayout>
-            <HomePage />
-          </MainLayout>
-        </Suspense>
-      ),
-    },
+        // Auth
+        ...authRoutes,
+        ...authDemoRoutes,
 
-    // Auth
-    ...authRoutes,
-    ...authDemoRoutes,
+        // Dashboard
+        ...dashboardRoutes,
 
-    // Dashboard
-    ...dashboardRoutes,
+        // Main
+        ...mainRoutes,
 
-    // Main
-    ...mainRoutes,
+        // No match
+        { path: '*', element: <Navigate to="/404" replace /> },
+      ];
 
-    // No match
-    { path: '*', element: <Navigate to="/404" replace /> },
-  ]);
+  return useRoutes(routes);
 }
