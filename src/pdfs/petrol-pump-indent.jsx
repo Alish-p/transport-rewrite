ccb53172a3237980e2407d74c0123ec539cc6aad
Alish Palasara <palasaraalis@gmail.com>
2025-10-07 15:55:51 +0530

    minor fixes

--- src/pdfs/petrol-pump-indent.jsx
+++ src/pdfs/petrol-pump-indent.jsx
@@ -31,7 +31,6 @@ export default function IndentPdf({ subtrip, tenant }) {
     _id,
     subtripNo,
     startDate,
-    expenses,
     initialAdvanceDiesel,
     initialAdvanceDieselUnit,
     initialTripAdvance,
