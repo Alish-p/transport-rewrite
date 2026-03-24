20c967dfa2e2509762db98f8219d7ea9c46fedd1
Alish Palasara <palasaraalis@gmail.com>
2026-03-24 21:51:38 +0530

    fix: update home chart image source from `home-chart-2.png` to `home-chart.png`.

--- src/sections/home/home-minimal.jsx
+++ src/sections/home/home-minimal.jsx
@@ -91,7 +91,7 @@ export function HomeMinimal({ sx, ...other }) {
         <Box
           component="img"
           alt="Home Chart"
-          src={`${CONFIG.site.basePath}/assets/images/home/home-chart-2.png`}
+          src={`${CONFIG.site.basePath}/assets/images/home/home-chart.png`}
           sx={{ width: 720 }}
         />
       </Box>
