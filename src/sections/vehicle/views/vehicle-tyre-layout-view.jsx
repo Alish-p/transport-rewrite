2e93c1bd70499e0e2348842267cafc2fb1677257
Zaara0511 <palasarazaara@gmail.com>
2026-04-19 23:03:51 +0530

    feat: add alert for non-owned vehicles in tyre layout view

--- src/sections/vehicle/views/vehicle-tyre-layout-view.jsx
+++ src/sections/vehicle/views/vehicle-tyre-layout-view.jsx
@@ -1,9 +1,19 @@
+import Alert from '@mui/material/Alert';
+
 import { CurrentTyreLayoutView } from './current-tyre-layout-view';
 import { VehicleTyreLayoutSelect } from './vehicle-tyre-layout-select';
 
 // ----------------------------------------------------------------------
 
 export function VehicleTyreLayoutView({ vehicle }) {
+    if (!vehicle?.isOwn) {
+        return (
+            <Alert severity="info" sx={{ m: 3 }}>
+                Tyre management is only for own vehicle
+            </Alert>
+        );
+    }
+
     if (vehicle?.tyreLayoutId) {
         return <CurrentTyreLayoutView vehicle={vehicle} />;
     }
