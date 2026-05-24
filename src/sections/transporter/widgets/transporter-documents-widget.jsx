5584a66a6606da180014458bc376bed44a7e7d5a
Zaara0511 <palasarazaara@gmail.com>
2026-05-24 18:02:14 +0530

    style: reorder hook imports in transporter documents widget

--- src/sections/transporter/widgets/transporter-documents-widget.jsx
+++ src/sections/transporter/widgets/transporter-documents-widget.jsx
@@ -1,5 +1,5 @@
 import { useDropzone } from 'react-dropzone';
-import { useState, useEffect, useCallback, useMemo } from 'react';
+import { useMemo, useState, useEffect, useCallback } from 'react';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
