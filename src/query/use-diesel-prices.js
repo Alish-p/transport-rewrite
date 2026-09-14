1bc5b317f0bd3b5efb8716e28f4adf349f400c8c
Zaara0511 <palasarazaara@gmail.com>
2026-09-15 01:13:09 +0530

    refactor: remove file-manager section and unused components

--- src/query/use-diesel-prices.js
+++
@@ -1,5 +0,0 @@
-// Deprecated: use the generic fuel price hooks instead.
-// This file is kept only to avoid breaking imports that might still exist.
-// Prefer importing from 'src/query/use-fuel-prices'.
-
-export {};
